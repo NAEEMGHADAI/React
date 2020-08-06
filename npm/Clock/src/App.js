3f63037b2a9d0582a2bf88278a6a42a1dfea905e
NAEEMGHADAI <51822103+NAEEMGHADAI@users.noreply.github.com>
2020-08-07 00:56:47 +1100

    Add files via upload

---
+++ npm/Clock/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Clock from "react-digital-clock";
+import "./App3.css";
+
+const App = () => {
+  return (
+    <div>
+      <Clock />
+    </div>
+  );
+};
+
+export default App;
---
+++ npm/Counter/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactCountdownClock from "react-countdown-clock";
+
+const App = () => {
+  return <ReactCountdownClock />;
+};
+
+export default App;
